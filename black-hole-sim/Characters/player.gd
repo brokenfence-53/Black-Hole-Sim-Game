f3a3316dba22086dfedf0d651126cd52c09198a7
brokenfence-53 <brokenfence444@gmail.com>
2026-04-13 17:36:04 -0400

    Fixed Dashed
    added sprite to see movement better

--- black-hole-sim/Characters/player.gd
+++ black-hole-sim/Characters/player.gd
@@ -6,20 +6,19 @@ const friction: float = 32.0
 
 #dash
 #how fash the character is going with a dash
-const dash_speed:float = 180.0
+const dash_speed: float = 5.0
 #how long the dash is in sec
-const dash_time: float = 0.12
+const dash_time: float = 0.25
 #if someone is dashing
 var dashing: bool = true
 #how long a player has been dashing for
 var dash_timer: float = 0.0
 #direction the player is dashing towards
-var dash_dir: Vector2 = Vector2.ZERO
+#var dash_dir: Vector2 = Vector2.ZERO
 #amt of time between dash
 var dash_reload_cost: float =0.5
 #how long a player has not been dashing for
 var dash_reload_timer: float = 0.0
-
 
 
 func _physics_process(delta: float) -> void:
@@ -27,7 +26,6 @@ func _physics_process(delta: float) -> void:
 		var input_movement: Vector2 = Vector2(
 			Input.get_action_strength("right") - Input.get_action_strength("left"), Input.get_action_strength("down") - Input.get_action_strength("up") 
 		).normalized()  ## making input return a 0/1/-1
-		
 		#creating velocity for both x,y so apply friction and accleration seperately to each axis
 		#exp and the -( is a power function so it tells the each fraction of speed change
 		# 1- makes it the amt applied into the amt remain
@@ -41,14 +39,16 @@ func _physics_process(delta: float) -> void:
 	_dash_logic(delta)
 	move_and_slide()
 func _dash_logic(delta: float) -> void:
+	var dash_dir: Vector2 = Vector2 (velocity.x,velocity.y)
 	if dashing and Input.is_action_just_pressed("dash"):
 		dashing = false
 		dash_timer = dash_time #starting dash timer
 		dash_reload_timer = dash_reload_cost
-		dash_dir = global_position.direction_to(get_local_mouse_position())
 		velocity = dash_dir * dash_speed
 	if dash_timer > 0.0:
 		dash_timer = max(0.0, dash_timer - delta)
+		if is_on_wall():
+			dash_timer = 0.0 #ends dash on a wall
 	else: 
 		if dash_reload_timer > 0.0:
 			dash_reload_timer -= delta
