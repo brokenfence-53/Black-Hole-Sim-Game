6cd3ff03e01f173f5fcc5bf49cb25027a5a5f7ac
brokenfence-53 <brokenfence444@gmail.com>
2026-04-13 09:11:17 -0700

    Adding Character Movement
    Script to Player Object

---
+++ black-hole-sim/Characters/player.gd
@@ -0,0 +1,10 @@
+extends CharacterBody2D
+@export var movement_speed : float = 100.0
+
+func _physics_process(delta: float) -> void:
+	var input_movement = Vector2 (
+		Input.get_action_strength("right") - Input.get_action_strength("left"),
+		Input.get_action_strength("down") - Input.get_action_strength("up") 
+	)
+	velocity = input_movement * movement_speed
+	
