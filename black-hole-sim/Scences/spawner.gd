1b6b492aab00830fdf156af5e14bd2331a163034
brokenfence-53 <brokenfence444@gmail.com>
2026-04-19 17:52:33 -0700

    some form of randomization from scale and y,x

--- black-hole-sim/Scences/spawner.gd
+++ black-hole-sim/Scences/spawner.gd
@@ -1,22 +1,42 @@
 extends Marker2D
-var enemynode = preload("res://Characters/enemy.tscn")
+@onready var enemynode : PackedScene = preload("res://Characters/enemy.tscn")
+var enemyamt: int = 0.00
+@export var point_1: Vector2 = Vector2(0,0)
+@export var point_2: Vector2 = Vector2(2000,1000)
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	randomize()
 	# Replace with function body.
 func _physics_process(delta: float) -> void:
-	if Input.is_action_just_pressed("mb"):
-		spawn(get_global_mouse_position())
-	
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+	##if Input.is_action_just_pressed("mb"):
+		##spawn(get_global_mouse_position())
 	pass
 
-func spawn(pos):
-	var instance = enemynode.instantiate()
-	instance.position = pos
-	scale = Vector2.ONE * randf_range(0.5,1.2)
-	add_child(instance)
+func get_random_point_inside(p1: Vector2, p2: Vector2) -> Vector2:
+	var x_value: float = randf_range(p1.x,p2.x)
+	var y_value: float = randf_range(p1.y,p2.y)
+	var random_point_inside: Vector2 = Vector2(x_value,y_value)
+	return(random_point_inside)
+
+
+func _spawn():
+	var enemyinstance: Node = enemynode.instantiate()
+	add_child(enemyinstance)
+	var randomposition: Vector2 = get_random_point_inside(point_1,point_2)
+	##var pos = instance.position 
+	scale = Vector2.ONE * randf_range(0.009,0.012)
+	enemyinstance.set_position(randomposition)
+	
+
+
+func _on_timer_timeout() -> void:
+	if enemyamt<5:
+		_spawn()
+		enemyamt +=1
+		print(enemyamt)
+	else:
+		pass
+		
+	
