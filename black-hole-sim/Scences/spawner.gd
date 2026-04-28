7855c3952d8ade38d06b44cef9dfed7fc21e012c
brokenfence-53 <brokenfence444@gmail.com>
2026-04-28 14:09:44 -0700

    trying to make it so they spawn in area

--- black-hole-sim/Scences/spawner.gd
+++ black-hole-sim/Scences/spawner.gd
@@ -1,10 +1,11 @@
-extends Area2D
+extends Node2D
 @export var enemy_prefab: PackedScene
 @export var target : CharacterBody2D 
 @export var enemymax: float 
 @export var enemyamt: float 
-@export var point_1: Vector2 = Vector2(-5000,-5000)
-@export var point_2: Vector2 = Vector2(5000,5000)
+@export var point_1: Vector2 = Vector2(-8000,-8000)
+@export var point_2: Vector2 = Vector2(8000,8000)
+var enemy_can_spawn: bool
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	randomize()
@@ -14,7 +15,6 @@ func _process(_delta: float) -> void:
 		return
 	else:
 		_spawn()
-		print(Global.enemyamt)
 
 func get_random_point_inside(p1: Vector2, p2: Vector2) -> Vector2:
 	var x_value: float = randf_range(p1.x,p2.x)
@@ -42,4 +42,19 @@ func _spawn():
 func _on_timer_timeout() -> void:
 	pass
 	
+	
+
+
+
+func _on_area_2d_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
+	if !Global.enemyamt<enemymax:
+		if body != enemy_prefab:
+				enemy_can_spawn = false
+				print("not in area")
+		else:
+			if body == enemy_prefab:
+				_spawn()
+				enemy_can_spawn = true
+				print("in area")
+
 	
